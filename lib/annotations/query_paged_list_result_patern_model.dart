34a7b10f3f84637719ba43043496b49b9801f75a
ahmet_guler <ahmetguler9595@gmail.com>
2025-05-16 02:40:11 +0300

    fixed annotion.

--- lib/annotations/query_paged_list_result_patern_model.dart
+++ lib/annotations/query_paged_list_result_patern_model.dart
@@ -1,3 +1,3 @@
 class QueryPagedListResultPaternModel {
-  QueryPagedListResultPaternModel();
+  const QueryPagedListResultPaternModel();
 }
--- lib/annotations/query_register_handler.dart
+++ lib/annotations/query_register_handler.dart
@@ -1,3 +1,3 @@
 class QueryRegisterHandler {
-  QueryRegisterHandler();
+  const QueryRegisterHandler();
 }
